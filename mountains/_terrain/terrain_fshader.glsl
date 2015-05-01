a03319bf5a1596efb3278fca649b0185fb0b25db
Menkid <johan_cattin@hotmail.com>
2015-05-01 23:26:23 +0200

    phong first try, need compilation check
    J'arrive pas a compiler sur mon windows, j'essayerai lundi

--- mountains/_terrain/terrain_fshader.glsl
+++ mountains/_terrain/terrain_fshader.glsl
@@ -5,8 +5,14 @@ uniform vec3 ka, kd, ks;
 uniform float p;
 
 out vec4 color;
+
 in vec2 uv;
 in vec3 newPos;
+
+in vec3 normal_mv;
+in vec3 light_dir;
+in vec3 view_dir;
+
 uniform sampler2D grass;
 uniform sampler2D snow;
 uniform sampler2D rock;
@@ -20,15 +26,28 @@ void main() {
 
 
 	if (newPos.y > 0.65) {
-		color = snowVec;
+		vec4 precolor = snowVec;
 	}
 	else if (newPos.y > 0.55 && newPos.y < 0.65){
-		color = mix(grassVec, snowVec, 0.2);
+		vec4 precolor = mix(grassVec, snowVec, 0.2);
 	}
 	else if (newPos.y < 0.375) {
-	color = rockVec;
+		vec4 precolor = rockVec;
 	}
 	else {
-		color = grassVec;
+		vec4 precolor = grassVec;
     }
+	
+	vec3 mirror_reflex = normalize(reflect(-light_dir, normal_mv));
+    vec3 ambiant = Ia * ka;
+    float NL = max(dot(normal_mv, light_dir), 0);
+    vec3 diffuse = (Id * kd) * NL;
+    float VR = max(dot(view_dir, mirror_reflex), 0);
+    vec3 specular = (Is * ks) * pow(VR, p);
+    vec3 phong = ambiant+diffuse+specular;
+	precolor.x *= phong.x;
+	precolor.y *= phong.y;
+	precolor.z *= phong.z;
+	color = precolor;
+	
 }
