2a41c544f6753550cc65b41e221ab41fdbd91c2a
Fabien Andre Zellweger <fazellwe@cf-ic@icin3pc51.epfl.ch>
2015-05-07 16:36:23 +0200

    correct the water transparency

--- mountains/_water/water_fshader.glsl
+++ mountains/_water/water_fshader.glsl
@@ -13,23 +13,22 @@ uniform float p;
 
 uniform sampler2D heightmap;
 
-const float transparency = 0.75f;
+const float transparency = 0.5f;
 
 
 void main() {
 	float height = texture(heightmap, uv).x;
-	if(height > seaLvl + 0.005){
-		color = vec4(0.2f, 0.2f, 0.f, 0.0f);
-	} else{
-		color = vec4(0.2f, 0.2f, 0.7f, transparency);
-	}
+
+	vec3 color_tex = vec3(0.2f, 0.2f, 0.7f);
 	
-	vec4 mirror_reflex = vec4(normalize(reflect(-light_dir, normal_mv)),transparency);
-    vec4 ambiant = vec4(Ia * (color*0.3),transparency);
+	
+	vec3 mirror_reflex = normalize(reflect(-light_dir, normal_mv));
+    vec3 ambiant = Ia * (color_tex*0.3);
     float NL = max(dot(normal_mv, light_dir), 0);
-    vec4 diffuse = vec4((Id * color) * NL,transparency);
+    vec3 diffuse = (Id * color_tex) * NL;
     float VR = max(dot(view_dir, mirror_reflex), 0);
-    vec4 specular = vec4((Is * (color*1.3)) * pow(VR, p),transparency);
-    color = ambiant+diffuse+specular;
+    vec3 specular = (Is * (color_tex*1.3)) * pow(VR, p);
+    color_tex = ambiant+diffuse+specular;
+    color = vec4(color_tex, transparency);
 	
 }
