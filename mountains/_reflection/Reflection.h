0b3c29842cc12c4b145fd36669ea5ee9778a330e
Walono <fabien.zellweger@gmail.com>
2015-05-21 13:30:19 +0200

    add incomplete reflection file

---
+++ mountains/_reflection/Reflection.h
@@ -0,0 +1,212 @@
+#pragma once
+#include "icg_common.h"
+
+
+class Reflection : public Material, public Light{
+protected:
+    GLuint _vao;          ///< vertex array object
+    GLuint _vbo_position; ///< memory buffer for positions
+    GLuint _vbo_index;    ///< memory buffer for indice
+    GLuint _pid;          ///< GLSL shader program ID
+    GLuint _heightmap;    ///< Texture ID
+    vector<GLuint> _tabMixingTex;
+	GLuint _tex1;		  //<- Texure
+	GLuint _tex2;		  //<- Texure
+	GLuint _tex3;		  //<- Texure
+	GLuint _tex4;
+    GLuint _num_indices;  ///< number of vertices to render
+    Light light;
+    
+public:    
+	void init(GLuint heightmapTex, vector<GLuint> tabMixingTex){
+        // Compile the shaders
+        _pid = opengp::load_shaders("_reflection/reflection_vshader.glsl", "_reflection/reflection_fshader.glsl");
+        if(!_pid) exit(EXIT_FAILURE);       
+        glUseProgram(_pid);
+        
+        // Vertex one vertex Array
+        glGenVertexArrays(1, &_vao);
+        glBindVertexArray(_vao);
+        
+        // Vertex coordinates and indices
+        {
+            std::vector<GLfloat> vertices;
+            std::vector<GLuint> indices;
+
+            int reflection_dim = 512;
+
+			//Generate the mesh            
+			for (int i = 0; i <= reflection_dim; i++){
+				for (int j = 0; j <= reflection_dim; j++){
+					vertices.push_back(-1.0f + i * 2 / (float)reflection_dim); vertices.push_back(-1.0f + j * 2 / (float)reflection_dim);
+				}
+			}
+
+			int indice = 0;
+			for (int row = 0; row<terrain_dim; row++){
+				for (int col = 0; col<reflection_dim; col++){
+					indices.push_back(indice);
+					indices.push_back(indice + 1);
+					indices.push_back(indice + reflection_dim + 2);
+					indices.push_back(indice);
+					indices.push_back(indice + reflection_dim + 1);
+					indices.push_back(indice + reflection_dim + 2);
+					indice++;
+				}
+				indice++;
+			}
+            _num_indices = indices.size();
+
+            // position buffer
+            glGenBuffers(1, &_vbo_position);
+            glBindBuffer(GL_ARRAY_BUFFER, _vbo_position);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
+
+            // vertex indices
+            glGenBuffers(1, &_vbo_index);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _vbo_index);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+
+            // position shader attribute
+            GLuint loc_position = glGetAttribLocation(_pid, "position");
+            glEnableVertexAttribArray(loc_position);
+            glVertexAttribPointer(loc_position, 2, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
+        }
+
+		///--- Load/Assign Hightmap texture
+		this->_heightmap = heightmapTex;
+		glBindTexture(GL_TEXTURE_2D, _heightmap);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		GLuint heightmap_id = glGetUniformLocation(_pid, "heightmap");
+		glUniform1i(heightmap_id, 0);
+
+		// Load texture
+		glGenTextures(1, &_tex1);		
+		glBindTexture(GL_TEXTURE_2D, _tex1);
+		glfwLoadTexture2D("_reflection/texture-herbe.tga", 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		GLuint grass_id = glGetUniformLocation(_pid, "grass");
+		glUniform1i(grass_id, 1 /*GL_TEXTURE1*/);
+
+		glGenTextures(1, &_tex2);
+		glBindTexture(GL_TEXTURE_2D, _tex2);
+		glfwLoadTexture2D("_reflection/snow.tga", 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		GLuint snow_id = glGetUniformLocation(_pid, "snow");
+		glUniform1i(snow_id, 2 /*GL_TEXTURE2*/);
+
+		glGenTextures(1, &_tex3);
+		glBindTexture(GL_TEXTURE_2D, _tex3);
+		glfwLoadTexture2D("_reflection/rock2.tga", 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		GLuint rock_id = glGetUniformLocation(_pid, "rock");
+		glUniform1i(rock_id, 3 /*GL_TEXTURE3*/);
+		
+		///bind texture for mix
+		this ->_tabMixingTex = tabMixingTex;
+		glActiveTexture(GL_TEXTURE4);
+		//glGenTextures(1, &_tabMixingTex.at(0));
+		glBindTexture(GL_TEXTURE_2D, _tabMixingTex.at(0));
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		GLuint mixTexTerrain_id = glGetUniformLocation(_pid, "mixTexTerrain");
+		glUniform1i(mixTexTerrain_id, 4);
+		
+		glActiveTexture(GL_TEXTURE6);
+		//glGenTextures(1, &_tabMixingTex.at(0));
+		glBindTexture(GL_TEXTURE_2D, _tabMixingTex.at(1));
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		GLuint mixSnowGrass_id = glGetUniformLocation(_pid, "mixSnowGrass");
+		glUniform1i(mixSnowGrass_id, 6);
+		
+		glGenTextures(1, &_tex4);
+		glBindTexture(GL_TEXTURE_2D, _tex4);
+		glfwLoadTexture2D("_reflection/montagneNeige2.tga", 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		GLuint grassSnow_id = glGetUniformLocation(_pid, "grassSnow");
+		glUniform1i(grassSnow_id, 5 /*GL_TEXTURE5*/);
+		
+		
+        	Material::setup(_pid);
+        Light::setup(_pid);
+
+        
+        // to avoid the current object being polluted
+        glBindVertexArray(0);
+		glUseProgram(0);
+    }
+           
+    void cleanup(){
+        glDeleteBuffers(1, &_vbo_position);
+        glDeleteBuffers(1, &_vbo_index);
+        glDeleteVertexArrays(1, &_vao);
+        glDeleteProgram(_pid);
+		glDeleteTextures(1, &_heightmap);
+    }
+    
+    void draw(const mat4& model, const mat4& view, const mat4& projection, float time, vec3 light_pos){
+        glUseProgram(_pid);
+        glBindVertexArray(_vao);
+        // Bind textures
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, _heightmap);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, _tex1);
+		glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, _tex2);
+		glActiveTexture(GL_TEXTURE3);
+		glBindTexture(GL_TEXTURE_2D, _tex3);
+		glActiveTexture(GL_TEXTURE4);
+		glBindTexture(GL_TEXTURE_2D,_tabMixingTex.at(0));
+		glActiveTexture(GL_TEXTURE5);
+		glBindTexture(GL_TEXTURE_2D,_tex4);
+		glActiveTexture(GL_TEXTURE6);
+		glBindTexture(GL_TEXTURE_2D,_tabMixingTex.at(1));
+		
+		Light::set_spot_direction(light_pos);
+		GLuint light_pos_id = glGetUniformLocation(_pid, "light_pos"); //Given in camera space
+		glUniform3fv(light_pos_id, ONE, Light::get_spot_direction(time).data());
+
+        // Setup MVP
+        mat4 P = projection;
+        mat4 MV = view*model;
+        GLuint P_id = glGetUniformLocation(_pid, "P");
+        glUniformMatrix4fv(P_id, 1, GL_FALSE, P.data());
+        
+        GLuint MV_id = glGetUniformLocation(_pid, "MV");
+        glUniformMatrix4fv(MV_id, 1, GL_FALSE, MV.data());
+
+
+        // Pass the current time stamp to the shader.
+        glUniform1f(glGetUniformLocation(_pid, "time"), time);
+        
+
+        // Draw
+        // TODO 5: For debugging it can be helpful to draw only the wireframe.
+        // You can do that by uncommenting the next line.
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        // TODO 5: Depending on how you set up your vertex index buffer, you
+        // might have to change GL_TRIANGLE_STRIP to GL_TRIANGLES.
+        glDrawElements(GL_TRIANGLES, _num_indices, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);        
+        glUseProgram(0);
+    }
+};
---
+++ mountains/_reflection/reflection_vshader.glsl
@@ -0,0 +1,64 @@
+#version 330 core
+
+
+uniform mat4 P;
+uniform mat4 MV;
+
+uniform sampler2D heightmap;
+uniform vec3 light_pos;
+
+in vec2 position;
+out vec2 uv;
+out vec3 newPos;
+
+out vec3 normal_mv;
+out vec3 light_dir;
+out vec3 view_dir;
+
+void main() {
+	mat4 mvp = P*MV;
+    uv = (position + vec2(1.0, 1.0)) * 0.5;
+	
+    float height = -texture(heightmap, uv).x +0.9;
+    vec3 pos_3d = vec3(position.x, height, position.y);
+    ///--phong shading---///
+    //vec4 vpoint_mv = MV * vec4(pos_3d, 1.0);
+    vec4 vpoint_mv = MV*vec4(pos_3d, 1.0);
+    
+    light_dir = normalize(light_pos - vpoint_mv.xyz);
+	view_dir = normalize(-vpoint_mv.xyz);
+	///---///
+    gl_Position = mvp * vec4(pos_3d, 1.0);
+	newPos = pos_3d.xyz;
+	
+
+	float delta = 1/512.0f; /// -> 1/128
+	vec2 v0xz = vec2(position.x, position.y - delta);
+	float h0 = texture(heightmap, (v0xz + vec2(1.0, 1.0)) * 0.5).x;
+	vec3 v0 = vec3(v0xz.x, h0, v0xz.y);
+	vec2 v1xz = vec2(position.x + delta, position.y);
+	float h1 = texture(heightmap, (v1xz + vec2(1.0, 1.0)) * 0.5).x;
+	vec3 v1 = vec3(v1xz.x, h1, v1xz.y);
+	vec2 v2xz = vec2(position.x, position.y - delta);
+	float h2 = texture(heightmap, (v2xz + vec2(1.0, 1.0)) * 0.5).x;
+	vec3 v2 = vec3(v2xz.x, h2, v2xz.y);
+	vec2 v3xz = vec2(position.x - delta, position.y);
+	float h3 = texture(heightmap, (v3xz + vec2(1.0, 1.0)) * 0.5).x;
+	vec3 v3 = vec3(v3xz.x, h3, v3xz.y);
+	
+	vec3 v01 = v1 * v0;
+	vec3 v12 = v2 * v1;
+	vec3 v23 = v3 * v2;
+	vec3 v30 = v0 * v3;
+	
+	vec3 vnormal = normalize(v01 + v12 + v23 + v30);
+	normal_mv = normalize((inverse(transpose(MV)) * vec4(vnormal, 1.0)).xyz);
+
+    //vec4 vpoint_mv = mv * vec4(position, 1.0);
+    //light_dir = normalize(light_pos - vpoint_mv.xyz);
+    //light_dir = normalize(light_pos - pos_3d);
+	//view_dir = normalize(-vpoint_mv.xyz);
+	//normal_mv = normalize((inverse(transpose(mv)) * vec4(vnormal, 1.0)).xyz);
+	//view_dir = -pos_3d;
+	//normal_mv = vnormal;
+}
